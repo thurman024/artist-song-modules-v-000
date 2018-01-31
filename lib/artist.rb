814cd4f22ae97911edb029e7194c140ad0d66372
Stephen Thurman <thurman024@gmail.com>
2018-01-31 16:23:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Artist
-  extend Memorable::ClassMethods
+  extend Memorable
   attr_accessor :name
   attr_reader :songs
 
--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,5 @@
 module Memorable
-  
+
     def reset_all
       self.all.clear
     end
